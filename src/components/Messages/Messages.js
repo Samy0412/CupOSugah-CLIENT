e89ff17ba236c0035cb3a6ffdfd37afc048505bc
Samy0412 <samantha.gadet@gmail.com>
2020-11-17 15:16:14 -0500

    trying to fix the messages attemps 100

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -107,7 +107,7 @@ useEffect(()=> {
     let timeStamp = conversations[conversationID][conversations[conversationID].length-1].time_sent;
     timeStamps ={...timeStamps, [receiver_id]: timeStamp};
      console.log("conversations[conversationsID]",conversations[conversationID])
-    for (let i=0; i< conversations[conversationID].length; i++) {
+    for (let i=conversations[conversationID].length-1; i>=0; i--) {
       
       let messageContent = (
         <div
