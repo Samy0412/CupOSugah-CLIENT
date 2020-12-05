0d9780946256cfd17dc13239d1acb18e12456a85
Samy0412 <samantha.gadet@gmail.com>
2020-12-05 15:35:01 -0500

    trying to fix localstorage

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -19,15 +19,16 @@ export default function useApplicationData() {
   //Gets the information from localstorage each time there is a refresh and set the state at first load)
   useEffect(() => {
     const data = localStorage.getItem("userObj");
-    const data2 = localStorage.getItem("databaseReset");
+    // const data2 = localStorage.getItem("databaseReset");
     if (data) {
       const user = JSON.parse(data);
-      const databaseReset = JSON.parse(data2);
-      setState({ ...state, user, databaseReset });
-    }else {
-      const databaseReset = JSON.parse(data2);
-      setState({ ...state, databaseReset })
+      // const databaseReset = JSON.parse(data2);
+      setState({ ...state, user});
     }
+    // }else {
+    //   const databaseReset = JSON.parse(data2);
+    //   setState({ ...state, databaseReset })
+    // }
   }, []);
 
   //Stores the user information in localStorage so that we can use it to set the state again if a refresh happens
@@ -35,9 +36,9 @@ export default function useApplicationData() {
     localStorage.setItem("userObj", JSON.stringify(state.user));
   }, [state.user]);
 
-  useEffect (()=> {
-    localStorage.setItem("databaseReset",JSON.stringify(state.databaseReset));
-  }, [state.databaseReset])
+  // useEffect (()=> {
+  //   localStorage.setItem("databaseReset",JSON.stringify(state.databaseReset));
+  // }, [state.databaseReset])
 
   return {
     state,
