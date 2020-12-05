0600e33453ba6624ce5f4cc0e04fc5406dd90f34
Samy0412 <samantha.gadet@gmail.com>
2020-12-05 14:13:26 -0500

    changed location of setTimer

--- src/components/App.js
+++ src/components/App.js
@@ -42,13 +42,6 @@ function App() {
     
   }, []);
 
-
-
-useEffect(()=>{
-    axios.get("https://cuposugah.herokuapp.com/api/debug/reset").then(()=>console.log("database reset!"))
-},[])
-
-
   //That is going to be our main app, once we log in or sign in
   const Website = () => (
     <div>
